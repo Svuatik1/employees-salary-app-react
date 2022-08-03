64b28c6490fb92552ace9a4ac24493401838ce85
Sviatoslav Yakymiv <sviatoslavyakymiv@MacBook-Air-Sviatoslav.local>
2022-08-03 22:30:40 +0300

    one

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to RELOAD.
         </p>
+        <p>asdass</p>
         <a
           className="App-link"
           href="https://reactjs.org"
