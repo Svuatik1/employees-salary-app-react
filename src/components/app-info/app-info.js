2549c1875f7a62bf3bd95ba31fa5cf7d460158ee
Sviatoslav Yakymiv <sviatoslavyakymiv@MacBook-Air-Sviatoslav.local>
2022-08-03 23:56:40 +0300

    changes 03/08/2022

---
+++ src/components/app-info/app-info.js
@@ -0,0 +1,13 @@
+import "./app-info.css";
+
+const AppInfo = () => {
+  return (
+    <div className="app-info">
+      <h1>Employees in company "N"</h1>
+      <h2>Total value employees: </h2>
+      <h2>Prize will be recived: </h2>
+    </div>
+  );
+};
+
+export default AppInfo;
---
+++ src/components/search-panel/search-panel.js
@@ -0,0 +1,13 @@
+import "./search-panel.css";
+
+const SearchPanel = () => {
+  return (
+    <input
+      type="text"
+      className="form-control search-input"
+      placeholder="Search coworker"
+    />
+  );
+};
+
+export default SearchPanel;
